15f7002ccbfbd28b1c738271361e363f13bd0791
Cecilia Terra <catreanden@gmail.com>
2024-10-25 18:22:16 +0000

    generador de dominos

---
+++ apps.js
@@ -0,0 +1,22 @@
+let pronoun = ['the', 'our' , 'your'];
+let adj = ['great', 'big' , 'extraordinary' , 'amazing'];
+let noun = ['jogger', 'racoon' , 'cat'];
+
+for (let i = 0 ; i < pronoun.length ; i++){
+    for (let a=0 ; a < adj.length ; a++){
+        for (let n = 0 ; n < noun.length ; n++ ){ 
+   
+    console.log(pronoun[i] + adj[a] + noun[n] + '.com')
+}
+}
+}
+
+for (let i = 0 ; i > pronoun.length ; i++){
+    for (let a=0 ; a > adj.length ; a++){
+        for (let n = 0 ; n > noun.length ; n++ ){ 
+   
+    console.log(pronoun[i] + adj[a] + noun[n] + '.es')
+}
+}
+}
+
