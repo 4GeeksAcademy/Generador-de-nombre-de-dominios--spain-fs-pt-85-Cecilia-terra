f74bddc9ebeabdb4462528fd9b4f33546dbe7118
Cecilia Terra <catreanden@gmail.com>
2024-10-25 18:56:38 +0000

    generador de dominios

--- apps.js
+++ apps.js
@@ -1,22 +1,17 @@
-let pronoun = ['the', 'our' , 'your'];
-let adj = ['great', 'big' , 'extraordinary' , 'amazing'];
-let noun = ['jogger', 'racoon' , 'cat'];
+ let pronoun = ['the', 'our' , 'your'];
+ let adj = ['great', 'big' , 'extraordinary' , 'amazing'];
+ let noun = ['jogger', 'racoon' , 'cat'];
+ let domainhacks =['.com' , '.net' , '.us' , '.io']
 
-for (let i = 0 ; i < pronoun.length ; i++){
-    for (let a=0 ; a < adj.length ; a++){
+ for (let i = 0 ; i < pronoun.length ; i++){
+     for (let a=0 ; a < adj.length ; a++){
         for (let n = 0 ; n < noun.length ; n++ ){ 
-   
-    console.log(pronoun[i] + adj[a] + noun[n] + '.com')
-}
-}
-}
-
-for (let i = 0 ; i > pronoun.length ; i++){
-    for (let a=0 ; a > adj.length ; a++){
-        for (let n = 0 ; n > noun.length ; n++ ){ 
-   
-    console.log(pronoun[i] + adj[a] + noun[n] + '.es')
-}
+  for (let d = 0; d < domainhacks.length;d++) {
+    console.log(pronoun[i] + adj[a] + noun[n]  + domainhacks[d] )
+    
+   } 
+     
+ }
 }
 }
 
